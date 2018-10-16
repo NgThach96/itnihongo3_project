dbfed7c9235acd5d908fd62c3d6828f79b84c647
manhn <manh199672@gmail.com>
2018-10-16 22:00:10 +0700

    create admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181007084222) do
+ActiveRecord::Schema.define(version: 20181016145541) do
+
+  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.bigint "review_id"
@@ -64,8 +70,6 @@ ActiveRecord::Schema.define(version: 20181007084222) do
     t.date "birthday"
     t.integer "gender"
     t.string "avatar"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "reviews"
